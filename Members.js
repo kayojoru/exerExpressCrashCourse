384652a569e63580105878411c5481c9785c5bc7
kayojoru <89625832+kayojoru@users.noreply.github.com>
2021-12-14 00:42:46 -0700

    Submit Express JS Assignment
    Submit Express JS Assignment

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Dope',
+        email: 'johnisnottaken@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'whataboutbob@aol.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Jannet Jackson',
+        email: 'betterthanmj@hotmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
